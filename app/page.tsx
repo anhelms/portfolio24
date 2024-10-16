a924a63000783ef4c51429436061bb4850bedc6d
Anna N Helms <unicornbuddha@gmail.com>
2024-10-16 02:45:08 -0700

    Adds Clients section to page and creates infinitemovingcards ui

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import Hero from '@/components/Hero';
 import Grid from '@/components/Grid';
 import RecentProjects from '@/components/RecentProjects';
+import Clients from '@/components/Clients';
 import { FloatingNav } from '@/components/ui/FloatingNavbar';
 import { navItems } from '@/data';
 
@@ -14,6 +15,7 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
+        <Clients />
       </div>
     </main>
   );
